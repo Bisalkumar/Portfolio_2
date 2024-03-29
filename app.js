29a3b4ac7172f03c4ca603db1e90f8d221841425
kritirajmaharana <kritirajmaharana@gmil.com>
2024-03-29 15:53:36 +0530

    kuch bhi test

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// gsap top scroll animation
 document.addEventListener("DOMContentLoaded", function() {
   var progressBar = document.getElementById('progress-bar');
   window.addEventListener('scroll', function() {
@@ -7,41 +6,39 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-
-
 function handleClick1() {
   document.getElementById('circle').style.top = '20px';
   document.getElementById('cir1').style.top = '20px';
-
 }
 function handleClick2() {
   document.getElementById('circle').style.top="100px";
   document.getElementById('cir1').style.top="100px";
-
-
 }
 function handleClick3() {
   document.getElementById('circle').style.top="180px";
   document.getElementById('cir1').style.top="180px";
-
 }
 function handleClick4() {
   document.getElementById('circle').style.top="260px";
   document.getElementById('cir1').style.top="260px";
-
 }
 function handleClick5() {
   document.getElementById('circle').style.top="340px";
   document.getElementById('cir1').style.top="340px";
-
 }
 function handleClick6() {
   document.getElementById('circle').style.top="420px";
   document.getElementById('cir1').style.top="420px";
-
 }
 function handleClick7() {
   document.getElementById('circle').style.top="500px";
   document.getElementById('cir1').style.top="500px";
+}
 
+function handleClick(index) {
+  var icons = document.querySelectorAll('.navigation .icon span');
+  icons.forEach(function(icon) {
+    icon.classList.remove('active');
+  });
+  icons[index - 1].classList.add('active');
 }
